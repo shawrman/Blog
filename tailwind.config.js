04ae7662195cf9d65a96db87523dbd9e1618d30c
shawrman <adir6111@gmail.com>
2022-06-16 08:50:55 +0300

    most of the things done
    need to finish the comments and add new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+    },
   },
   plugins: [],
 }
