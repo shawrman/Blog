d9b279be9b8c7bcc19c9881a55b923efa6a639f0
shawrman <adir6111@gmail.com>
2022-06-19 08:08:08 +0300

    Update Comments.jsx

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -18,8 +18,6 @@ const Comments = ({ slug }) => {
       {comment.length > 0 && (
         <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
           <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-           
-            {' '}
             Comments
           </h3>
           {comments.map((comment) =>
